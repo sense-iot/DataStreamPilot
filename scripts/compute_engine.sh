e9f6f5194d3019dc1c2b93ced4f9481094a7be17
waasnipun <waasnipun@gmail.com>
2023-12-13 17:24:27 +0200

    testing compute node

--- scripts/compute_engine.sh
+++ scripts/compute_engine.sh
@@ -1,37 +1,3 @@
-# #!/usr/bin/env bash
-
-# source setup.sh
-# source ${SENSE_SCRIPTS_HOME}/setup_env.sh
-
-# build_wireless_firmware ${COMPUTE_ENGINE_HOME} ${COMPUTE_ENGINE_EXE_NAME}
-# build_status=$?
-# if [ $build_status -ne 0 ]; then
-#     exit $build_status
-# fi
-
-# if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
-#   cp ${COMPUTE_ENGINE_HOME}/bin/${ARCH}/${COMPUTE_ENGINE_EXE_NAME}.elf ${SENSE_FIRMWARE_HOME}
-
-#   flash_firmware ${COMPUTE_ENGINE_EXE_NAME} ${COMPUTE_ENGINE_NODE}
-  
-#   #create_tap_interface "${COMPUTE_ENGINE_NODE}" &
-  
-#   export COMPUTE_ENGINE_ROUTER_UP=1
-
-#   echo "aiocoap-client coap://[2001:660:5307:3107:a4a9:dc28:5c45:38a9]/riot/board"
-#   echo "coap info"
-#   echo "coap get [2001:660:5307:3107:a4a9:dc28:5c45:38a9]:5683 /.well-known/core"
-#   echo "coap get [2001:660:5307:3107:a4a9:dc28:5c45:38a9]:5683 /riot/board"
-#   echo "coap get 192.168.2.135:5683 /.well-known/core"
-#   echo "coap get example.com:5683 /.well-known/core # with sock dns"
-#   echo "coap get [2001:660:5307:3107:a4a9:dc28:5c45:38a9]:5683 /temperature"
-
-#   echo "nc m3-${COMPUTE_ENGINE_NODE} 20000"
-#   nc m3-${COMPUTE_ENGINE_NODE} 20000
-
-# fi
-
-
 #!/usr/bin/env bash
 
 source setup.sh
@@ -87,3 +53,38 @@ if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
 
     stop_jobs "${compute_engine_node_job_id}" "${n_node_job_id}" "${border_router_job_id}"
 fi
+
+
+
+# source setup.sh
+# source ${SENSE_SCRIPTS_HOME}/setup_env.sh
+
+# build_wireless_firmware ${COMPUTE_ENGINE_HOME} ${COMPUTE_ENGINE_EXE_NAME}
+# build_status=$?
+# if [ $build_status -ne 0 ]; then
+#     exit $build_status
+# fi
+
+# if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
+#   cp ${COMPUTE_ENGINE_HOME}/bin/${ARCH}/${COMPUTE_ENGINE_EXE_NAME}.elf ${SENSE_FIRMWARE_HOME}
+
+#   flash_firmware ${COMPUTE_ENGINE_EXE_NAME} ${COMPUTE_ENGINE_NODE}
+  
+#   #create_tap_interface "${COMPUTE_ENGINE_NODE}" &
+  
+#   export COMPUTE_ENGINE_ROUTER_UP=1
+
+#   echo "aiocoap-client coap://[2001:660:5307:3107:a4a9:dc28:5c45:38a9]/riot/board"
+#   echo "coap info"
+#   echo "coap get [2001:660:5307:3107:a4a9:dc28:5c45:38a9]:5683 /.well-known/core"
+#   echo "coap get [2001:660:5307:3107:a4a9:dc28:5c45:38a9]:5683 /riot/board"
+#   echo "coap get 192.168.2.135:5683 /.well-known/core"
+#   echo "coap get example.com:5683 /.well-known/core # with sock dns"
+#   echo "coap get [2001:660:5307:3107:a4a9:dc28:5c45:38a9]:5683 /temperature"
+
+#   echo "nc m3-${COMPUTE_ENGINE_NODE} 20000"
+#   nc m3-${COMPUTE_ENGINE_NODE} 20000
+
+# fi
+
+

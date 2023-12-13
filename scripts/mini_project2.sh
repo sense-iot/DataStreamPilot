b1009b7d3d3b4f795277d607918f7a94a3d70650
waasnipun <waasnipun@gmail.com>
2023-12-13 03:02:07 +0200

    adding a new node

--- scripts/mini_project2.sh
+++ scripts/mini_project2.sh
@@ -5,7 +5,7 @@ source ${SENSE_SCRIPTS_HOME}/setup_env.sh
 
 EXPERIMENT_NAME="mini-project-2-group-12"
 M3_NODE_COUNT=2
-A8_NODE_COUNT=2
+A8_NODE_COUNT=1
 EXPERIMENT_ID=0;
 
 if ! is_experiment_running "${EXPERIMENT_NAME}"; then
@@ -31,22 +31,25 @@ fi
 
 
 export GNRC_NETWORKING_NODE=${a8_nodes[0]}
-export MQTT_CLIENT_NODE=${a8_nodes[1]}
+# export MQTT_CLIENT_NODE=${a8_nodes[1]}
 
 export BORDER_ROUTER_NODE=${m3_nodes[0]}
-export SENSOR_CONNECTED_NODE=${m3_nodes[1]}
+# export SENSOR_CONNECTED_NODE=${m3_nodes[1]}
+export COMPUTE_ENGINE_NODE=${m3_nodes[1]}
 
 
-write_variable_to_file "MQTT_CLIENT_NODE" "$MQTT_CLIENT_NODE"
+# write_variable_to_file "MQTT_CLIENT_NODE" "$MQTT_CLIENT_NODE"
 write_variable_to_file "BORDER_ROUTER_NODE" "$BORDER_ROUTER_NODE"
 write_variable_to_file "GNRC_NETWORKING_NODE" "$GNRC_NETWORKING_NODE"
-write_variable_to_file "SENSOR_CONNECTED_NODE" "$SENSOR_CONNECTED_NODE"
+# write_variable_to_file "SENSOR_CONNECTED_NODE" "$SENSOR_CONNECTED_NODE"
+write_variable_to_file "COMPUTE_ENGINE_NODE" "$COMPUTE_ENGINE_NODE"
 
 printf "%-50s %s\n" "DataStereamPilot: GNRC_NETWORKING_NODE:" "a8 - $GNRC_NETWORKING_NODE"
-printf "%-50s %s\n" "DataStereamPilot: MQTT_CLIENT_NODE:" "a8 - $MQTT_CLIENT_NODE"
+# printf "%-50s %s\n" "DataStereamPilot: MQTT_CLIENT_NODE:" "a8 - $MQTT_CLIENT_NODE"
 
 printf "%-50s %s\n" "DataStereamPilot: BORDER_ROUTER_NODE:" "m3 - $BORDER_ROUTER_NODE"
-printf "%-50s %s\n" "DataStereamPilot: SENSOR_CONNECTED_NODE:" "m3 - $SENSOR_CONNECTED_NODE"
+# printf "%-50s %s\n" "DataStereamPilot: SENSOR_CONNECTED_NODE:" "m3 - $SENSOR_CONNECTED_NODE"
+printf "%-50s %s\n" "DataStereamPilot: COMPUTE_ENGINE_NODE:" "m3 - $COMPUTE_ENGINE_NODE"
 
 # printf "%-25s %s\n" "COAP_SERVER_NODE:" "$COAP_SERVER_NODE"
 # printf "%-25s %s\n" "SENSOR_CONNECTED_NODE:" "$SENSOR_CONNECTED_NODE"
@@ -57,11 +60,13 @@ printf "%-50s %s\n" "DataStereamPilot: SENSOR_CONNECTED_NODE:" "m3 - $SENSOR_CON
 # printf "%-25s %s\n" "SITE:" "$SENSE_SITE"
 
 echo "======================================================== $ARCH"
-# source ${SENSE_SCRIPTS_HOME}/gnrc_border_router.sh
-# echo "======================================================== $ARCH"
-# source ${SENSE_SCRIPTS_HOME}/gnrc_networking.sh
+source ${SENSE_SCRIPTS_HOME}/gnrc_border_router.sh
+echo "======================================================== $ARCH"
+source ${SENSE_SCRIPTS_HOME}/gnrc_networking.sh
 # echo "======================================================== $ARCH"
 # source ${SENSE_SCRIPTS_HOME}/mqtt_broker_setup.sh
+# echo "======================================================== $ARCH"
+# source ${SENSE_SCRIPTS_HOME}/emcute_mqttsn.sh
 echo "======================================================== $ARCH"
-source ${SENSE_SCRIPTS_HOME}/emcute_mqttsn.sh
+source ${SENSE_SCRIPTS_HOME}/compute_engine.sh
 echo "======================================================== $ARCH"

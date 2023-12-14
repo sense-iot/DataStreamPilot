4a048bf538ea2635fee857fc64ce81d679606eb8
Dilafdo <mdilanfdo1995@gmail.com>
2023-12-14 22:43:30 +0200

    fix

--- src/denoiser/main.c
+++ src/denoiser/main.c
@@ -18,6 +18,8 @@ int is_outlier(float readings[NUM_SENSORS], float z_threshold) {
         mean_reading += readings[i];
     }
     mean_reading /= NUM_SENSORS;
+    int16_t mean_reading_int = (int16_t)(mean_reading * 100);
+    DEBUG_PRINT("Mean = %i\n", mean_reading_int);
 
     // Calculate standard deviation of the readings
     float std_dev_reading = 0.0;
@@ -25,11 +27,17 @@ int is_outlier(float readings[NUM_SENSORS], float z_threshold) {
         std_dev_reading += (float)pow((double)(readings[i] - mean_reading), 2);  
     }
     std_dev_reading = sqrt(std_dev_reading / NUM_SENSORS);
+    int16_t std_dev_reading_int = (int16_t)(std_dev_reading * 1000);
+    DEBUG_PRINT("SD = %i\n", std_dev_reading_int);
 
     // Calculate z-score for each sensor
     int is_outlier = 0;
     for (int i = 0; i < NUM_SENSORS; i++) {
         float z_score = fabs((readings[i] - mean_reading) / std_dev_reading);
+
+        int16_t z_score_int = (int16_t)(z_score * 1000);
+        DEBUG_PRINT("Z score = %i\n", z_score_int);
+
         if (z_score > z_threshold) {
             is_outlier = 1;
             break;
@@ -43,30 +51,21 @@ int main(void) {
     ztimer_sleep(ZTIMER_SEC, 1);
     puts("Hello from RIOT!");
 
-    char temp1_str[10];
-
     // Assuming one set of readings from each sensor at a specific time
-    // float sensor_readings[NUM_SENSORS] = {25.5, 25.3, 25.6};  // Replace with actual readings
-    int16_t sensor_value = 25.5;
-
-    DEBUG_PRINT("Program started.. \n");
-
-    sprintf(temp1_str, "%i", sensor_value);
-    DEBUG_PRINT("Processed Reading: Sensor1=%s\n", temp1_str);
+    int16_t sensor_readings[NUM_SENSORS] = {4150, 4350, 4210};  // Replace with actual readings
 
     // Set z-score threshold (adjust based on your requirements)
-    // float z_threshold = 2.0;
+    float z_threshold = 2.0;
 
-    // // Check for outliers
-    //  if (!is_outlier(sensor_readings, z_threshold)) {
-    //      // Process the readings (replace with your processing logic)
-    //      sprintf(temp1_str, "%f", (double)sensor_readings[0]);
-    //      DEBUG_PRINT("Processed Reading: Sensor1=%s, Sensor2=%f, Sensor3=%f\n",
-    //             temp1_str, (double)sensor_readings[1], (double)sensor_readings[2]);
-    //  } else {
-    //      // Handle outlier case
-    //      DEBUG_PRINT("Outlier detected. Ignoring the reading.\n");
-    //  }
+    // Check for outliers
+     if (!is_outlier(sensor_readings, z_threshold)) {
+         // Process the readings (replace with your processing logic)
+         DEBUG_PRINT("Processed Reading: Sensor1 = %i, Sensor2 = %i, Sensor3 = %i\n",
+                sensor_readings[0], sensor_readings[1], sensor_readings[2]);
+     } else {
+         // Handle outlier case
+         DEBUG_PRINT("Outlier detected. Ignoring the reading.\n");
+     }
 
     return 0;
 }

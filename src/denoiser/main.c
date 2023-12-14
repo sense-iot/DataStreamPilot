0eb019fbf8df667b7a4588c3b9efed5a50faf9d1
Dilafdo <mdilanfdo1995@gmail.com>
2023-12-14 23:49:03 +0200

    fix

--- src/denoiser/main.c
+++ src/denoiser/main.c
@@ -11,7 +11,7 @@
 #define NUM_SENSORS 3
 
 // Function to check for outliers using z-scores
-int is_outlier(int16_t readings[NUM_SENSORS], float z_threshold) {
+int filter_outliers(int16_t readings[NUM_SENSORS], float z_threshold) {
     // Calculate mean of the readings
     float mean_reading = 0.0;
     for (int i = 0; i < NUM_SENSORS; i++) {
@@ -29,24 +29,29 @@ int is_outlier(int16_t readings[NUM_SENSORS], float z_threshold) {
     }
     std_dev_reading = sqrt(std_dev_reading / NUM_SENSORS);
 
-    int16_t sd = std_dev_reading * 100;   // Debugging
-    printf("SD = %i\n", (int)sd);       // Debugging
+    int sd = std_dev_reading * 100;     // Debugging
+    printf("SD = %i\n", sd);           // Debugging
 
-    // Calculate z-score for each sensor
-    int is_outlier = 0;
+    // Filtering outliers
+    int sensors_count = NUM_SENSORS;
+    float new_mean_reading = 0.0;
     for (int i = 0; i < NUM_SENSORS; i++) {
+
+        // Calculate z-score
         float z_score = fabs((readings[i] - mean_reading) / std_dev_reading);
 
-        int16_t z_score_int = (int16_t)(z_score * 1000);   // Debugging   
-        DEBUG_PRINT("Z score = %i\n", z_score_int);     // Debugging
+        int16_t z_score_int = (int16_t)(z_score * 1000);    // Debugging   
+        DEBUG_PRINT("Z score = %i\n", z_score_int);         // Debugging
 
         if (z_score > z_threshold) {
-            is_outlier = 1;
-            break;
+            sensors_count--;
+        } else {
+            new_mean_reading += readings[i];
         }
     }
-    DEBUG_PRINT("is_outlier: %i\n", is_outlier);
-    return is_outlier;
+    new_mean_reading /= sensors_count
+    printf("new mean: %i\n", (int)new_mean_reading);    // Debugging
+    return (int16_t)new_mean_reading;
 }
 
 int main(void) {
@@ -60,14 +65,17 @@ int main(void) {
     float z_threshold = 1.3;
 
     // Check for outliers
-     if (!is_outlier(sensor_readings, z_threshold)) {
-         // Process the readings (replace with your processing logic)
-         DEBUG_PRINT("Processed Reading: Sensor1 = %i, Sensor2 = %i, Sensor3 = %i\n",
-                sensor_readings[0], sensor_readings[1], sensor_readings[2]);
-     } else {
-         // Handle outlier case
-         DEBUG_PRINT("Outlier detected. Ignoring the reading.\n");
-     }
+    //  if (!is_outlier(sensor_readings, z_threshold)) {
+    //      // Process the readings (replace with your processing logic)
+    //      DEBUG_PRINT("Processed Reading: Sensor1 = %i, Sensor2 = %i, Sensor3 = %i\n",
+    //             sensor_readings[0], sensor_readings[1], sensor_readings[2]);
+    //  } else {
+    //      // Handle outlier case
+    //      DEBUG_PRINT("Outlier detected. Ignoring the reading.\n");
+    //  }
+
+    int16_t avg_temp = filter_outliers(sensor_readings, z_threshold);
+    printf("Average temperature: %i\n", avg_temp);
 
     return 0;
 }

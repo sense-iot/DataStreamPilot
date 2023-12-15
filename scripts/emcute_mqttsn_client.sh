efbedc2cf7103e1e79a00b06a8adb067cc4cf11b
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-12-15 11:05:44 +0100

    sensor cache

--- scripts/emcute_mqttsn_client.sh
+++ scripts/emcute_mqttsn_client.sh
@@ -3,15 +3,15 @@
 source setup.sh
 source ${SENSE_SCRIPTS_HOME}/setup_env.sh
 
-# if [ "$PREV_BROKER_IP" != "$BROKER_IP" ]; then
-#   echo "DataStereamPilot: The broker IP has changed ${BROKER_IP}."
-#   export PREV_BROKER_IP=${BROKER_IP}
-#   build_wireless_firmware ${EMCUTE_MQTSSN_HOME} ${EMCUTE_MQTSSN_EXE_NAME} iotlab-m3
-# else
-#   echo "DataStereamPilot: The broker IP has not changed ${BROKER_IP}."
-#   export PREV_BROKER_IP=${BROKER_IP}
-#   build_wireless_firmware_cached ${EMCUTE_MQTSSN_HOME} ${EMCUTE_MQTSSN_EXE_NAME} iotlab-m3
-# fi
+if [ "$PREV_BROKER_IP" != "$BROKER_IP" ]; then
+  echo "DataStereamPilot: The broker IP has changed ${BROKER_IP}."
+  export PREV_BROKER_IP=${BROKER_IP}
+  build_wireless_firmware ${EMCUTE_MQTSSN_HOME} ${EMCUTE_MQTSSN_EXE_NAME} iotlab-m3
+else
+  echo "DataStereamPilot: The broker IP has not changed ${BROKER_IP}."
+  export PREV_BROKER_IP=${BROKER_IP}
+  build_wireless_firmware_cached ${EMCUTE_MQTSSN_HOME} ${EMCUTE_MQTSSN_EXE_NAME} iotlab-m3
+fi
 
 build_wireless_firmware ${EMCUTE_MQTSSN_CLIENT_HOME} ${EMCUTE_MQTSSN_EXE_NAME} iotlab-m3
 build_status=$?
@@ -21,7 +21,7 @@ fi
 
 if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
   cp ${EMCUTE_MQTSSN_CLIENT_HOME}/bin/iotlab-m3/${EMCUTE_MQTSSN_CLIENT_EXE_NAME}.elf ${SENSE_FIRMWARE_HOME}
-  # cp ${EMCUTE_MQTSSN_CLIENT_HOME}/bin/iotlab-m3/${EMCUTE_MQTSSN_CLIENT_EXE_NAME}.elf ${SENSE_HOME}/release/${EMCUTE_MQTSSN_CLIENT_EXE_NAME}_${EMCUTE_ID}.elf
+  cp ${EMCUTE_MQTSSN_CLIENT_HOME}/bin/iotlab-m3/${EMCUTE_MQTSSN_CLIENT_EXE_NAME}.elf ${SENSE_HOME}/release/${EMCUTE_MQTSSN_CLIENT_EXE_NAME}_${EMCUTE_ID}.elf
 
   echo "Flashing new firmware for ${ARCH} node : ${MQTT_CLIENT_NODE}"
   flash_firmware ${EMCUTE_MQTSSN_CLIENT_EXE_NAME} ${MQTT_CLIENT_NODE}

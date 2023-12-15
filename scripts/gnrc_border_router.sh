0025faf530ce0464781d38f8f41fae513636458f
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-12-15 08:21:55 +0100

    cache elf files

--- scripts/gnrc_border_router.sh
+++ scripts/gnrc_border_router.sh
@@ -9,19 +9,26 @@ fi
 source setup.sh
 source ${SENSE_SCRIPTS_HOME}/setup_env.sh
 
-echo "build_wireless_firmware_cached ${BORDER_ROUTER_HOME} ${BORDER_ROUTER_EXE_NAME} iotlab-m3"
+file_to_check= ${SENSE_HOME}/release/gnrc_border_router.elf
 
-build_wireless_firmware_cached ${BORDER_ROUTER_HOME} ${BORDER_ROUTER_EXE_NAME} iotlab-m3
-build_status=$?
-if [ $build_status -ne 0 ]; then
-  exit $build_status
+if [ ! -f "$file_to_check" ]; then
+  echo "build_wireless_firmware_cached ${BORDER_ROUTER_HOME} ${BORDER_ROUTER_EXE_NAME} iotlab-m3"
+  build_wireless_firmware_cached ${BORDER_ROUTER_HOME} ${BORDER_ROUTER_EXE_NAME} iotlab-m3
+  build_status=$?
+  if [ $build_status -ne 0 ]; then
+    exit $build_status
+  fi
+  ELF_FILE=${BORDER_ROUTER_HOME}/bin/${ARCH}/${BORDER_ROUTER_EXE_NAME}.elf
+else
+  echo "File exists: $file_to_check"
+  ELF_FILE=$file_to_check
 fi
 
 if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
-  cp ${BORDER_ROUTER_HOME}/bin/${ARCH}/${BORDER_ROUTER_EXE_NAME}.elf ${SENSE_FIRMWARE_HOME}
+  cp $ELF_FILE ${SENSE_FIRMWARE_HOME}
 
   flash_firmware ${BORDER_ROUTER_EXE_NAME} ${BORDER_ROUTER_NODE}
   create_tap_interface "${BORDER_ROUTER_NODE}" &
-  
+
   export SENSE_BORDER_ROUTER_UP=1
 fi
--- scripts/gnrc_networking.sh
+++ scripts/gnrc_networking.sh
@@ -8,16 +8,29 @@ fi
 source setup.sh
 source ${SENSE_SCRIPTS_HOME}/setup_env.sh
 
-build_wireless_firmware_cached ${GNRC_NETWORKING_HOME} ${GNRC_NETWORKING_EXE_NAME} iotlab-a8-m3
-build_status=$?
-if [ $build_status -ne 0 ]; then
-  exit $build_status
+
+# File to check
+file_to_check= ${SENSE_HOME}/release/gnrc_networking.elf
+
+# Check if the file exists
+if [ ! -f "$file_to_check" ]; then
+    build_wireless_firmware_cached ${GNRC_NETWORKING_HOME} ${GNRC_NETWORKING_EXE_NAME} iotlab-a8-m3
+  build_status=$?
+  if [ $build_status -ne 0 ]; then
+    exit $build_status
+  fi
+  ELF_FILE=${BORDER_ROUTER_HOME}/bin/${ARCH}/${BORDER_ROUTER_EXE_NAME}.elf
+else
+  echo "File exists: $file_to_check"
+  ELF_FILE=$file_to_check
 fi
+
+
 
 if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
 
-  cp ${GNRC_NETWORKING_HOME}/bin/iotlab-a8-m3/${GNRC_NETWORKING_EXE_NAME}.elf ${SENSE_FIRMWARE_HOME}
-  cp ${GNRC_NETWORKING_HOME}/bin/iotlab-a8-m3/${GNRC_NETWORKING_EXE_NAME}.elf ~/A8
+  cp $ELF_FILE ${SENSE_FIRMWARE_HOME}
+  cp $ELF_FILE ~/A8
 
   echo "Flashing new firmware for iotlab-a8-m3 node : ${GNRC_NETWORKING_NODE}"
   ssh root@node-a8-${GNRC_NETWORKING_NODE} 'bash -s' <${SENSE_HOME}/src/network/gnrc_networking_a8/flash.sh

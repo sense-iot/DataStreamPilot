1c926565531a26510ebe8ff3ff7d819795080b80
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2023-12-21 23:54:28 +0100

    add status of second border router

--- scripts/status.sh
+++ scripts/status.sh
@@ -23,16 +23,18 @@ MQTT_CLIENT_NODE_1=$(read_variable_from_file "MQTT_CLIENT_NODE_1")
 MQTT_CLIENT_NODE_2=$(read_variable_from_file "MQTT_CLIENT_NODE_2")
 MQTT_CLIENT_NODE_3=$(read_variable_from_file "MQTT_CLIENT_NODE_3")
 BORDER_ROUTER_NODE=$(read_variable_from_file "BORDER_ROUTER_NODE")
-GNRC_NETWORKING_NODE=$(read_variable_from_file "GNRC_NETWORKING_NODE")
+BORDER_ROUTER_NODE_2=$(read_variable_from_file "BORDER_ROUTER_NODE_2")
 SENSOR_CONNECTED_NODE=$(read_variable_from_file "SENSOR_CONNECTED_NODE")
 DENOISER_NODE=$(read_variable_from_file "DENOISER_NODE")
 DENOISER_NODE_TEST=$(read_variable_from_file "DENOISER_NODE_TEST")
+BORDER_ROUTER_NODE_a8=$(read_variable_from_file "BORDER_ROUTER_NODE_a8")
 
 printf "%-50s %s\n" "DataStereamPilot: GNRC_NETWORKING_NODE:" "a8 - $GNRC_NETWORKING_NODE"
 printf "%-50s %s\n" "DataStereamPilot: DENOISER_NODE:" "a8 - $DENOISER_NODE"
-
+printf "%-50s %s\n" "DataStereamPilot: BORDER_ROUTER_NODE_a8:" "a8 - $BORDER_ROUTER_NODE_a8"
 
 printf "%-50s %s\n" "DataStereamPilot: BORDER_ROUTER_NODE:" "m3 - $BORDER_ROUTER_NODE"
+printf "%-50s %s\n" "DataStereamPilot: BORDER_ROUTER_NODE_2:" "m3 - $BORDER_ROUTER_NODE_2"
 # printf "%-50s %s\n" "DataStereamPilot: SENSOR_CONNECTED_NODE:" "m3 - $SENSOR_CONNECTED_NODE"
 printf "%-50s %s\n" "DataStereamPilot: MQTT_CLIENT_NODE_1:" "m3 - $MQTT_CLIENT_NODE_1"
 printf "%-50s %s\n" "DataStereamPilot: MQTT_CLIENT_NODE_2:" "m3 - $MQTT_CLIENT_NODE_2"
@@ -43,6 +45,7 @@ printf "%-50s %s\n" "DataStereamPilot: DENOISER_NODE_TEST:" "m3 - $DENOISER_NODE
 echo " ----- M3 - A8 nodes ----- "
 echo "ssh root@node-a8-${GNRC_NETWORKING_NODE}  # mqtt broker"
 echo "ssh root@node-a8-${DENOISER_NODE}    # denoiser"
+echo "ssh root@node-a8-${BORDER_ROUTER_NODE_a8}    # a8 border router"
 
 echo " ----- M3 nodes ----- "
 echo "nc m3-${BORDER_ROUTER_NODE} 20000         # border router"

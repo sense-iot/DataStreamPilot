2893e649c825cc57d3f3d2bc24b08b300ebdcc18
waasnipun <waasnipun@gmail.com>
2023-12-26 17:42:42 +0200

    Update InfluxDB host IP address

--- src/server/app/configuration.py
+++ src/server/app/configuration.py
@@ -5,7 +5,7 @@ import os
 # ------------------------------  from database.py file ------------------------------ # 
 # InfluxDB credentials
 #HOST      = os.environ.get('INFLUXDB_HOST', '192.168.1.172')
-HOST     = os.environ.get('INFLUXDB_HOST', 'localhost')
+HOST     = os.environ.get('INFLUXDB_HOST', '172.17.0.1')
 PORT     = os.environ.get('INFLUXDB_PORT', 8086)
 USERNAME = os.environ.get('INFLUXDB_USER', 'iotmini2')
 PASSWORD = os.environ.get('INFLUXDB_USER_PASSWORD', 'sense')

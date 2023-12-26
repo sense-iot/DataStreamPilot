e8c6d64906ff58a25e028b09a62fed38b2370a84
Rukshan Perera <2173530+KRVPerera@users.noreply.github.com>
2023-12-26 23:34:35 +0200

    Update node names and formatting in status.sh script

--- scripts/status.sh
+++ scripts/status.sh
@@ -20,7 +20,7 @@ echo "DataStreamPilot: a8 nodes: ${a8_nodes[@]}"
 echo "DataStreamPilot: m3 nodes: ${m3_nodes[@]}"
 
 GNRC_NETWORKING_NODE=$(read_variable_from_file "GNRC_NETWORKING_NODE")
-COMPUTER_ENGINE_NODE=$(read_variable_from_file "COMPUTER_ENGINE_NODE")
+COMPUTE_ENGINE_NODE=$(read_variable_from_file "COMPUTE_ENGINE_NODE")
 
 MQTT_CLIENT_NODE_1=$(read_variable_from_file "MQTT_CLIENT_NODE_1")
 MQTT_CLIENT_NODE_2=$(read_variable_from_file "MQTT_CLIENT_NODE_2")
@@ -28,23 +28,23 @@ MQTT_CLIENT_NODE_3=$(read_variable_from_file "MQTT_CLIENT_NODE_3")
 BORDER_ROUTER_NODE=$(read_variable_from_file "BORDER_ROUTER_NODE")
 DENOISER_NODE=$(read_variable_from_file "DENOISER_NODE")
 
-printf "%-50s %s\n" "DataStreamPilot: GNRC_NETWORKING_NODE: " "a8 - $GNRC_NETWORKING_NODE"
-printf "%-50s %s\n" "DataStreamPilot: BORDER_ROUTER_NODE: " "m3 - $BORDER_ROUTER_NODE"
-printf "%-50s %s\n" "DataStreamPilot: DENOISER_NODE: " "m3 - $DENOISER_NODE"
-printf "%-50s %s\n" "DataStreamPilot: MQTT_CLIENT_NODE_1: " "m3 - $MQTT_CLIENT_NODE_1"
-printf "%-50s %s\n" "DataStreamPilot: MQTT_CLIENT_NODE_2: " "m3 - $MQTT_CLIENT_NODE_2"
-printf "%-50s %s\n" "DataStreamPilot: MQTT_CLIENT_NODE_3: " "m3 - $MQTT_CLIENT_NODE_3"
-printf "%-50s %s\n" "DataStreamPilot: COMPUTER_ENGINE_NODE: " "m3 - $COMPUTER_ENGINE_NODE"
+printf "%-50s %s\n" "DataStreamPilot: GNRC_NETWORKING_NODE  :   " "a8 - $GNRC_NETWORKING_NODE"
+printf "%-50s %s\n" "DataStreamPilot: BORDER_ROUTER_NODE    :   " "m3 - $BORDER_ROUTER_NODE"
+printf "%-50s %s\n" "DataStreamPilot: DENOISER_NODE         :   " "m3 - $DENOISER_NODE"
+printf "%-50s %s\n" "DataStreamPilot: MQTT_CLIENT_NODE_1    :   " "m3 - $MQTT_CLIENT_NODE_1"
+printf "%-50s %s\n" "DataStreamPilot: MQTT_CLIENT_NODE_2    :   " "m3 - $MQTT_CLIENT_NODE_2"
+printf "%-50s %s\n" "DataStreamPilot: MQTT_CLIENT_NODE_3    :   " "m3 - $MQTT_CLIENT_NODE_3"
+printf "%-50s %s\n" "DataStreamPilot: COMPUTE_ENGINE_NODE   :   " "m3 - $COMPUTE_ENGINE_NODE"
 
 echo " ----- M3 - A8 nodes ----- "
 echo "ssh root@node-a8-${GNRC_NETWORKING_NODE}  # mqtt broker"
 
 echo " ----- M3 nodes ----- "
-echo "nc m3-${BORDER_ROUTER_NODE} 20000   # border router"
-echo "nc m3-${DENOISER_NODE} 20000   # denoiser node"
-echo "nc m3-${MQTT_CLIENT_NODE_1} 20000   # sensor 1"
-echo "nc m3-${MQTT_CLIENT_NODE_2} 20000   # sensor 2"
-echo "nc m3-${MQTT_CLIENT_NODE_3} 20000    # sensor 3"
-echo "nc m3-${COMPUTER_ENGINE_NODE} 20000 # COMPUTER_ENGINE_NODE"
+printf "nc m3-%03d 20000   # border router node\n" $BORDER_ROUTER_NODE
+printf "nc m3-%03d 20000   # denoiser node\n" $DENOISER_NODE
+printf "nc m3-%03d 20000   # sensor_1 node\n" $MQTT_CLIENT_NODE_1
+printf "nc m3-%03d 20000   # sensor_2 node\n" $MQTT_CLIENT_NODE_2
+printf "nc m3-%03d 20000   # sensor_3 node\n" $MQTT_CLIENT_NODE_3
+printf "nc m3-%03d 20000   # computer engine node\n" $COMPUTE_ENGINE_NODE
 
 echo "Broker IP 1 : $(extract_global_ipv6)"

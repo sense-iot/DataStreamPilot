daac10106b94bd36bdd3b1ab5f3711469e645313
Rukshan Perera <2173530+KRVPerera@users.noreply.github.com>
2023-12-26 23:51:49 +0200

    Add conditional statements to only publish messages if arguments are provided

--- scripts/broker_pub.sh
+++ scripts/broker_pub.sh
@@ -4,11 +4,23 @@ source setup.sh
 source ${SENSE_SCRIPTS_HOME}/setup_env.sh
 
 ip_address=$(extract_global_ipv6)
-echo "mosquitto_pub -h $ip_address -p 1886 -t s1 -m $1"
-mosquitto_pub -h $ip_address -p 1886 -t s1 -m $1 -q 2
 
-echo "mosquitto_pub -h $ip_address -p 1886 -t s2 -m $2"
-mosquitto_pub -h $ip_address -p 1886 -t s2 -m $2
+if [ -n "$1" ]; then 
+    echo "mosquitto_pub -h $ip_address -p 1886 -t s1 -m $1"
+    mosquitto_pub -h $ip_address -p 1886 -t s1 -m $1
+fi
 
-echo "mosquitto_pub -h $ip_address -p 1886 -t s3 -m $3"
-mosquitto_pub -h $ip_address -p 1886 -t s3 -m $3
+if [ -n "$2" ]; then 
+    echo "mosquitto_pub -h $ip_address -p 1886 -t s2 -m $2"
+    mosquitto_pub -h $ip_address -p 1886 -t s2 -m $2
+fi
+
+if [ -n "$3" ]; then 
+    echo "mosquitto_pub -h $ip_address -p 1886 -t s3 -m $3"
+    mosquitto_pub -h $ip_address -p 1886 -t s3 -m $3
+fi
+
+if [ -n "$4" ]; then
+    echo "mosquitto_pub -h $ip_address -p 1886 -t d -m $4"
+    mosquitto_pub -h $ip_address -p 1886 -t d -m $4
+fi

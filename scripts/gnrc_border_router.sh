3d3f53bae17b6d6d970c6d77cc4001b911167584
Rukshan Perera <2173530+KRVPerera@users.noreply.github.com>
2023-12-31 18:12:16 +0200

    Update gnrc_border_router.sh and setup_env.sh scripts

--- scripts/gnrc_border_router.sh
+++ scripts/gnrc_border_router.sh
@@ -1,37 +1,29 @@
 #!/usr/bin/env bash
 
-if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
-  if [ -n "$SENSE_BORDER_ROUTER_UP" ]; then
-    return 0
-  fi
-fi
-
 source setup.sh
 source ${SENSE_SCRIPTS_HOME}/setup_env.sh
 
-file_to_check=${SENSE_HOME}/release/gnrc_border_router.elf
+RELEASE_FILE=${SENSE_HOME}/release/gnrc_border_router.elf
+build_path=${BORDER_ROUTER_HOME}/bin/${ARCH}/${BORDER_ROUTER_EXE_NAME}.elf
+
 echo "============== Building on channel ${NODE_CHANNEL} ================="
-if [ ! -f "$file_to_check" ]; then
+if [ ! -f "$RELEASE_FILE" ]; then
   echo "DataStreamPilot: build_wireless_firmware_cached ${BORDER_ROUTER_HOME} ${BORDER_ROUTER_EXE_NAME} iotlab-m3 ${DEFAULT_CHANNEL}"
   build_wireless_firmware_cached ${BORDER_ROUTER_HOME} ${BORDER_ROUTER_EXE_NAME} iotlab-m3 ${DEFAULT_CHANNEL}
   build_status=$?
   if [ $build_status -ne 0 ]; then
     exit $build_status
   fi
-  ELF_FILE=${BORDER_ROUTER_HOME}/bin/${ARCH}/${BORDER_ROUTER_EXE_NAME}.elf
+  cp $build_path $RELEASE_FILE
 else
-  echo "DataStreamPilot: File exists: $file_to_check"
-  ELF_FILE=$file_to_check
+  echo "DataStreamPilot: File exists: $RELEASE_FILE"
 fi
 
 if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
-  cp $ELF_FILE ${SENSE_FIRMWARE_HOME}
-  cp $ELF_FILE ${SENSE_HOME}/release/
+  flash_elf ${RELEASE_FILE} ${BORDER_ROUTER_NODE}
 
-  # flash_firmware ${BORDER_ROUTER_EXE_NAME} ${BORDER_ROUTER_NODE}
-
-  border_router_job_id=$(submit_border_router_job "${BORDER_ROUTER_NODE}")
-  wait_for_job "${border_router_job_id}"
+  # border_router_job_id=$(submit_border_router_job "${BORDER_ROUTER_NODE}")
+  # wait_for_job "${border_router_job_id}"
 
   current_ethos_id=$(ps -ef | grep ethos | grep -v "grep" | grep perera | awk '{print $2}' | head -1)
   if [ -z "$current_ethos_id" ]; then
@@ -41,6 +33,4 @@ if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
     echo "DataStreamPilot: Killing Ethos process ID $current_ethos_id"
     kill -9 $current_ethos_id
   fi
-
-  export SENSE_BORDER_ROUTER_UP=1
 fi

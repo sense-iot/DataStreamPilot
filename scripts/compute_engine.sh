d982d9d953996b84d59c94f847d4a3c11c088d5e
Rukshan Perera <2173530+KRVPerera@users.noreply.github.com>
2023-12-31 18:03:19 +0200

    Update release file name and fix flashing firmware

--- scripts/compute_engine.sh
+++ scripts/compute_engine.sh
@@ -3,32 +3,29 @@
 source setup.sh
 source ${SENSE_SCRIPTS_HOME}/setup_env.sh
 
-file_to_check=${SENSE_HOME}/release/${COMPUTE_ENGINE_EXE_NAME}_${SENSOR_ID}.elf
+RELEASE_FILE=${SENSE_HOME}/release/${COMPUTE_ENGINE_EXE_NAME}_${SENSOR_ID}_${SENSE_SITE}.elf
+build_path=${COMPUTE_ENGINE_HOME}/bin/${ARCH}/${COMPUTE_ENGINE_EXE_NAME}.elf
 my_arch=${ARCH}
 
 # Check if the file exists
-if [ ! -f "$file_to_check" ]; then
+if [ ! -f "$RELEASE_FILE" ]; then
     build_wireless_firmware ${COMPUTE_ENGINE_HOME} ${COMPUTE_ENGINE_EXE_NAME} ${ARCH} ${NODE_CHANNEL}
     build_status=$?
     if [ $build_status -ne 0 ]; then
         exit $build_status
     fi
-    ELF_FILE=${COMPUTE_ENGINE_HOME}/bin/${ARCH}/${COMPUTE_ENGINE_EXE_NAME}.elf
+    cp $build_path $RELEASE_FILE
 else
-    echo "DataStreamPilot: File exists: $file_to_check"
-    ELF_FILE=$file_to_check
+    echo "DataStreamPilot: File exists: $RELEASE_FILE"
 fi
 
 if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
-    cp $ELF_FILE ${SENSE_FIRMWARE_HOME}
-    cp $ELF_FILE ${SENSE_HOME}/release/${COMPUTE_ENGINE_EXE_NAME}_${SENSOR_ID}.elf
 
     if [ "$my_arch" = "iotlab-m3" ]; then
         echo "Flashing new firmware for ${my_arch} node : ${COMPUTE_ENGINE_NODE}"
-        # flash_elf ${SENSE_HOME}/release/${COMPUTE_ENGINE_EXE_NAME}_${SENSOR_ID}.elf ${COMPUTE_ENGINE_NODE}
+        flash_elf ${RELEASE_FILE} ${COMPUTE_ENGINE_NODE}
 
-        iotlab-experiment submit -n "${COMPUTE_ENGINE_EXE_NAME}_${SENSOR_ID}" -d ${EXPERIMENT_TIME} -l ${SENSE_SITE},m3,${COMPUTE_ENGINE_NODE},${SENSE_HOME}/release/${COMPUTE_ENGINE_EXE_NAME}_${SENSOR_ID}.elf
-        echo "nc m3-${COMPUTE_ENGINE_NODE} 20000"
+        # iotlab-experiment submit -n "${COMPUTE_ENGINE_EXE_NAME}_${SENSOR_ID}" -d ${EXPERIMENT_TIME} -l ${SENSE_SITE},m3,${COMPUTE_ENGINE_NODE},${SENSE_HOME}/release/${COMPUTE_ENGINE_EXE_NAME}_${SENSOR_ID}.elf
         #nc m3-${COMPUTE_ENGINE_NODE} 20000
     elif [ "$my_arch" = "iotlab-a8-m3" ]; then
         cp $ELF_FILE ~/A8/${COMPUTE_ENGINE_EXE_NAME}_${SENSOR_ID}_a8.elf
@@ -46,5 +43,4 @@ if [ -n "$IOT_LAB_FRONTEND_FQDN" ]; then
     fi
 
     echo "nc m3-${COMPUTE_ENGINE_NODE} 20000"
-
 fi
